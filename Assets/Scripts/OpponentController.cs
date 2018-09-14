f5f6e2d34c5cf75ba022b72a2df4e9d7aa240d67
ytksy <ytksy1218@gmail.com>
2018-09-14 20:25:51 +0900

    my first commit

---
+++ Assets/Scripts/OpponentController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class OpponentController : MonoBehaviour
+{
+	public Transform target;
+	public Transform homeTarget;
+ 	
+	private NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (PlayerController.isPowerUp)
+		{
+			agent.SetDestination(homeTarget.position);
+		}
+		else
+		{
+			agent.SetDestination(target.position);
+		}
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			if (PlayerController.isPowerUp)
+			{
+				gameObject.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+//using UnityEditor.Experimental.Build.AssetBundle;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speedX;
+    public float speedZ;
+//    public float powerUpSpan;
+    private float time = 0.0f;
+//    public Transform respawnPoint;
+
+    public float warpOffset;
+    
+//    public Text countText;
+    static public int energyBallCount = 0;
+    static public int allEnergyBallCount;
+    
+//    public Text winText;
+    static public bool stageClear;
+//    public Text stageNameText;
+
+    private Rigidbody rb;
+    static public int count;
+
+    private string direction;
+    private Vector3 dirVector;
+    static public bool isPowerUp;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.velocity = new Vector3(speedX, 0.0f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.velocity = new Vector3(-speedX, 0.0f, 0.0f);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.velocity = new Vector3(0.0f, 0.0f, speedZ);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rb.velocity = new Vector3(0.0f, 0.0f, -speedZ);
+
+        }
+    }
+
+    void PutBomb()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+}
